a94a345b484c7c175837b9968e7b975c14e1a303
R-Stanz <stanz@tutamail.com>
2024-04-01 18:28:31 -0300

    Intro Slides In PDF and Simple Function Example

---
+++ classes/01-Intro/codes/hello_worlds_with_simple_function/hello_world.c
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+#include <stdio.h>
+
+void print_hello() {
+	printf("Hello World!\n");
+}
+
+int main () {
+	
+	print_hello();
+
+	return 0;
+}
---
+++ classes/01-Intro/codes/hello_worlds_with_simple_function/hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void print_hello() {
+	cout << "Hello World!" << endl;
+}
+
+int main() {
+
+	print_hello();
+
+	return 0;
+}
