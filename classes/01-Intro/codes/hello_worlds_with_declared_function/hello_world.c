dae03f1f5adc986717a12308b89642be61a05e72
R-Stanz <stanz@tutamail.com>
2024-04-01 18:46:35 -0300

    Hello Worlds With Declared Functions

---
+++ classes/01-Intro/codes/hello_worlds_with_declared_function/hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_hello();
+
+int main() {
+
+	print_hello();
+
+	return 0;
+}
+
+void print_hello() {
+	printf("Hello World!\n");
+}
---
+++ classes/01-Intro/codes/hello_worlds_with_declared_function/hello_world.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void print_hello();
+
+int main() {
+
+	print_hello();
+
+	return 0;
+}
+
+void print_hello() {
+	cout << "Hello World!" << endl;
+}
