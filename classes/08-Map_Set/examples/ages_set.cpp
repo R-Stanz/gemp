ef98aac285b1feec2bb751bb0fbf64a0f0bd8fdc
R-Stanz <stanz@tutamail.com>
2024-05-20 20:02:31 -0300

    Default C++ Map and Set - Set Example

---
+++ classes/08-Map_Set/examples/ages_set.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+	set<int> idades{23, 25};
+
+	while (true) {
+
+	int idade;
+	cin >> idade;
+
+	if (idade < 16)
+	    break;
+
+	idades.insert(idade);
+	}
+
+	cout << "Imprimindo idades" << endl;
+	for (auto idade = idades.rbegin(); idade != idades.rend(); idade++)
+		cout << *idade << endl;
+
+	return 0;
+}
